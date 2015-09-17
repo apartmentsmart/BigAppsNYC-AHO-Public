3a1b8f2d132fae25d336ab1150e1699cd83c59cf
Joshua Cappell <j.b.cappell@gmail.com>
2015-09-17 13:16:53 -0400

    merging Josh's and Chris' work.

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -30,109 +30,155 @@
     FastClick.attach(document.body);
   }
 
-app.factory('filterState', function() {
+/**
+  -- Sets, Gets, and Mutates cross-controller search filter data.
+**/
+app.factory('globalFilter', function() {
  var savedData = {}
+ 
  function set(data) {
    savedData = data;
  }
+ 
  function get() {
   return savedData;
  }
 
+ function mutate(scope){
+
+
+  if(scope.thisSearch.borough == 'All')
+      delete scope.thisSearch.borough; 
+  
+  if(scope.thisSearch.age >= 55){
+      scope.thisSearch.elderly = 1;
+  }
+
+  delete scope.thisSearch.age;
+
+
+  if(scope.thisSearch.disabilityStatus != "None")
+      scope.thisSearch.disabled = 1;
+
+  delete scope.thisSearch.disabilityStatus;
+
+  delete scope.thisSearch.sizeOfHousehold;
+
+
+ }
+
  return {
   set: set,
-  get: get
+  get: get,
+  mutate: mutate
  }
 
 });
 
-//Search Controller Working Protoype
-app.controller('searchController', ['$scope', 'filterState', function($scope, filterState) {
-      
+app.factory('dataHandler', ['$http', '$filter', function($http, $filter){
 
-      //set $scope.search default variable if $scope.search is not set
-      (!$scope.search)
-        $scope.search = { borough:"All", sizeOfHousehold:2, disabilityStatus:"None"};
-       
-      
-        //watch $scope.search attributes for change
-        $scope.$watchGroup(['search.borough', 'search.sizeOfHousehold'], function(newValues, oldValues, scope){
-
-            filterState.set($scope.search);
-            console.log(filterState.get());
-            console.log('!!');
-        
-        });
-
-
-
-
-}]);
-
-app.controller('resultsController', ['$scope','$http','$filter','filterState', '$state', function($scope, $http, $filter, filterState, $state) {
-
-             //Init Listings Object
-       $scope.listings = [];
-       $scope.search = filterState.get();
-
-       if(!$scope.search.borough)
-        $scope.search = $state.params;
-
-      console.log($scope.search);
-
-
-       //Call local demo JSON
-       $http.get("/data/nycdemodata.json").success(function (data, status, headers, config) {
+      /*
+        --Fetches, and optionally Filters, JSON object.
+        --@scope is the current scope of the controller which needs to be passed in order to assign the return of $http call.
+        --@scopeAtt is the attribute to assign the returned $http data to in the scope of the controller.
+        --@scopeFilter is an optional object of key-value pairs. If passed only data that matches the conditions will be returned to the controller.
+      */
+      function fetch(scope, scopeAtt, scopeFilter){
+       $http.get("/data/nycdemodata.json")
+       .success(function (data) {  
           
-        console.log(data);
+          scope[scopeAtt]= data; 
+          
+          if(scopeFilter){ 
+               scope[scopeAtt] = filter( scope[scopeAtt], scopeFilter);
+          }
 
-          //On Success set listings object equal to json return
-          $scope.listings = data;
-          if($scope.search)          
-            $scope.filteredResults = filterSearch();
-          else
-            $scope.filteredResults = "No Filter";
-
-          console.log($scope.filteredResults);
-
-         }).error(function (data, status, headers, config) {
-           //on Error log error status
-           console.log(status)
-           $scope.listings = "error";
-       });
-
-      function filterSearch(){  
-
-        return $filter('filter')($scope.listings, {borough: $scope.search.borough});
+        })
         
+        .error(function (error) { 
+
+             scope[scopeAtt] = error; });;
+        
+        }
+
+
+      /*
+       --Filters JSON object if called by Fetch.
+       --@data a JSON object
+       --@filter is an object of key-value pairs. Data that matches the conditions will be returned to the controller.
+      */
+      function filter(data, filter){
+       return $filter('filter')(data, filter);
+      }
+
+
+      return{
+        fetch: fetch,
+        filter: filter
       }
 
 }]);
 
-app.controller('listingController', ['$scope', '$http', '$filter', 'filterState', '$state', function($scope, $http, $filter, filterState, $state){
+//Handles Searching Actions
+app.controller('searchController', ['$scope', 'globalFilter', function($scope, globalFilter) {
+
+  //set $scope.search default variable if $scope.search is not set
+  (!$scope.search)
+    $scope.search = { borough:"All", sizeOfHousehold:1, disabilityStatus:"None"};
 
 
-           //Call local demo JSON
-     $http.get("/data/nycdemodata.json").success(function (data, status, headers, config) {
-        
-      console.log(data);
+  //watch $scope.search attributes for change
+  $scope.$watchGroup(['search.borough', 'search.sizeOfHousehold'], function(newValues, oldValues, scope){
+    
 
-        //On Success set listings object equal to json return
-        $scope.listings = data;
-        
-        $scope.thisListing = $filter('filter')($scope.listings, {hud_id: $state.params.id});
+    /**
+      TODO: Handle all search param modulation here BEFORE handed off to any other controller..
+    **/
 
 
-        console.log($scope.thisListing);
+    globalFilter.set($scope.search);
+  });
 
-       }).error(function (data, status, headers, config) {
-         //on Error log error status
-         console.log(status)
-         $scope.listings = "error";
-     });
+}]);
+
+//Handles Search Result Actions
+app.controller('resultsController', ['$scope','globalFilter', 'dataHandler', '$state', function($scope, globalFilter, dataHandler, $state) {
+
+  //Init Listings Object
+  $scope.listings={};
+
+  //Attempts to set Search Object
+  $scope.thisSearch = globalFilter.get();
+
+  //If no Search Object, set to URL params.
+  if(!$scope.thisSearch.borough)
+    $scope.thisSearch = $state.params;
 
 
 
+
+   //Handle search param mutation
+   globalFilter.mutate($scope);
+
+   
+
+  //Get data that matches the $scope.search object and assign it to $scope.listings
+  dataHandler.fetch($scope, "listings", $scope.thisSearch);
+
+
+}]);
+
+//Handles Listing Actions
+app.controller('listingController', ['$scope','globalFilter', 'dataHandler', '$state', function($scope, globalFilter, dataHandler, $state) {
+
+  //Init thisListing Object
+  $scope.thisListing={};
+
+  //Set Search Object based on $state.params
+  $scope.search = {hud_id: $state.params.id};
+
+  //get data that matches the $scope.search object and assign it to $scope.thisListing;
+  dataHandler.fetch($scope, "thisListing", $scope.search);
 
 }]);
 
