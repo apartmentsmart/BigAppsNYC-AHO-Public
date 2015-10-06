2d4a9bf028ba1bece2ff944f77a7ec1909c1f8c4
Joshua Cappell <j.b.cappell@gmail.com>
2015-10-06 15:04:37 -0400

    User Dash and Notification Action functionality

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -140,8 +140,6 @@ app.factory('dataHandler', ['$http', '$filter', 'globalFilter', function($http, 
           
           scope[scopeAtt] = data; 
 
-          console.log(data);
-
         });
     }
     }
@@ -311,7 +309,7 @@ app.controller('listingController', ['$scope','globalFilter', 'dataHandler', '$s
 
 
 //Handles Search Result Actions
-app.controller('dashController', ['$scope', 'dataHandler', '$state', function($scope, dataHandler, $state) {
+app.controller('dashController', ['$scope', 'dataHandler', '$state','$filter', function($scope, dataHandler, $state, $filter) {
 
   //Init Notifications Object
   $scope.notifications = {};
@@ -324,13 +322,56 @@ app.controller('dashController', ['$scope', 'dataHandler', '$state', function($s
   //Get data that matches the $scope.search object and assign it to $scope.notifications
   dataHandler.fetch($scope, "notifications", $scope.thisSearch);
 
-  //Method to Dismiss
+  //Method to Dismiss or Archive Notifications
+  $scope.handleNotification = function(params){
 
-  //Method to Archive
+
+    var thisNotification = $filter('filter')($scope.notifications,{'id':params.id})[0];
+    
+    thisNotification[params.action]= 1;
+    thisNotification[params.action+"_on"] = new Date();
+
+
+
+  }
+
 
 }]);
 
 
-
+/**
+ **Directive to dynamically add click functions to buttons.
+ **Currently used exclusively to direct and dismiss notifications.
+ ****
+ **Returns a button template and assigns the ng-click attribute to be handled by the link function(actionHandler), 
+ **and associates the button display text as dictated by the $scope.notifications model.
+ **"uiaction" is for any click function that would in normal circumstances use ui-sref. 
+ **"controllerParams" and "controllerFunction" handle any click function from a controller.
+ **/
+app.directive('actionButton', ['$location', function(location) {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {controllerParams: '@', uiAction: '@', actionText: '@', controllerFunction: '='},
+        template: function(tElement, tAttrs) {
+            return '<button class="button dark" ng-click="actionHandler()"  actionText="{{actionText}}">{{actionText}}</button>';
+        },
+        transclude: true,
+        link: function(scope, element, attrs) {
+            
+            scope.actionHandler = function() {
+                
+                if(attrs.uiaction){
+                  location.url(attrs.uiaction);
+                }
+                else if(attrs.controllerFunction && attrs.controllerParams){
+                  var params = eval(attrs.controllerParams);
+                  scope.controllerFunction(params);
+                }
+            
+            };
+        }
+    };
+}]);
 
 })(); 
