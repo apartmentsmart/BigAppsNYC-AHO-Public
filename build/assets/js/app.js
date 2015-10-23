4f6077547b17cbc8fe8512880d4868805b12d396
Joshua Cappell <j.b.cappell@gmail.com>
2015-10-23 15:12:56 -0400

    Note Functionality

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -20730,9 +20730,6 @@ angular.module('application').controller('listingController', ['$scope','globalF
 
       $scope.thisListing = d;
 
-      console.log($scope.thisListing)
-
-
   });
 
 
@@ -20851,7 +20848,6 @@ angular.module('application').controller('listingController', ['$scope','globalF
 
   $scope.convertDate = function (stringDate){
     stringDate = stringDate.replace(' ', 'T');
-    console.log(stringDate+"Z")
     var dateOut = new Date(stringDate);
     dateOut.setDate(dateOut.getDate() + 1);
     return dateOut;
@@ -20865,6 +20861,60 @@ angular.module('application').controller('listingController', ['$scope','globalF
 
 }]);
 
+//Handles Listing Actions
+angular.module('application').controller('noteController', ['$scope','globalFilter','dataService', '$sce', '$state', 
+  function($scope, globalFilter, dataService, $sce, $state) {
+
+  	$scope.note = {};
+
+    if(globalFilter.get('fbResponse').id){
+		
+		var fbresponse = globalFilter.get('fbResponse');
+
+
+	    var accountEndpoint =  "http://api.affordablehousingonline.com/nyc/user/"+fbresponse.id+"/";
+
+	    dataService.async(accountEndpoint).then(function(d){
+				$scope.account = d[0];
+	    		
+				noteEndpoint = "http://api.affordablehousingonline.com/nyc/note/";
+				$scope.note = {user:$scope.account.id,listing:$state.params.id}
+
+				dataService.async(noteEndpoint, $scope.note).then(function(r){
+
+					console.log(r)
+					if(!r.applied_on){
+						r.applied_on = new Date();
+					}
+					else
+						r.applied_on = $scope.convertDate(r.applied_on)
+					
+					$scope.note = r;
+					
+
+				});
+					
+
+	    });
+
+	}
+
+	$scope.saveNote = function(){
+
+		console.log($scope.note);
+
+	}
+
+
+  $scope.convertDate = function (stringDate){
+    stringDate = stringDate.replace(' ', 'T');
+    var dateOut = new Date(stringDate);
+    dateOut.setDate(dateOut.getDate() + 1);
+    return dateOut;
+  }
+
+
+}]);
 //Handles Search Result Actions
 angular.module('application').controller('resultsController', ['$scope','globalFilter', '$state', 'dataService', function($scope, globalFilter, $state, dataService) {
 
